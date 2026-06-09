25fb0fc825930577952f5b38278a569bd0d57f65
Josu Alzueta <josu.alzueta@jakala.com>
2026-06-09 15:19:03 +0200

    chore: Adds @ alias to src folder

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
 	},
 });
